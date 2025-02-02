45026ec6a10f1d83807164d5778b57147133c8b7
bterral <baptisteterral@gmail.com>
2025-02-02 20:03:29 +0100

    Added basic movements

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
-public class BoatMovement
+public class BoatMovement : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
     
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal and vertical inputs
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        // Allows moving 10 meters per second instead of 10 meters per frame
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        // Move the boat
+        transform.Translate(0, 0, translation);
+        // Rotate the boat
+        transform.Rotate(0, rotation, 0);
+    }
 }

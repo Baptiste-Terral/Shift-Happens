ad791cb0bf5955c221a767b58e4cd6044a6387f4
Joffrey <fouche.joffrey@gmail.com>
2025-04-18 15:42:06 +0200

    fix log

--- Assets/Scripts/ProcGen/MagnitudeGen.cs
+++ Assets/Scripts/ProcGen/MagnitudeGen.cs
@@ -67,7 +67,7 @@ public class MagnitudeGen : MonoBehaviour
         }
 
         TidalGenerator.Instance.onMagnitudeGen.Invoke(m_noiseMaps);
-        Debug.Log("Notify update");
+        //Debug.Log("Notify update");
         m_canUpdateMap = true;
     }
 

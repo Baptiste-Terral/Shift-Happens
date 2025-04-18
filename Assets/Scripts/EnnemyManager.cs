56d092e39679f76231637d25ad379f40a264c2cd
joffrey <145475446+joffrey3@users.noreply.github.com>
2025-04-18 12:08:10 +0200

    18/04/2025 second push

--- Assets/Scripts/EnnemyManager.cs
+++ Assets/Scripts/EnnemyManager.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 public class EnnemyManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int timer = 0;
     [SerializeField] private GameObject ennemyGameObject;
     [SerializeField] private List<Ennemy> allEnnemyType = new List<Ennemy>();
     [SerializeField] private List<EnnemyBehaviour> allEnnemyInScene = new List<EnnemyBehaviour>();
     void Start()
     {
+
         for(int i = 0; i < allEnnemyType.Count; i++)
         {
-            print(allEnnemyType[i]);
+            //Vector2 position = ChooseRandomPosition();
             Generate(allEnnemyType[i],new Vector2(i,i));
         }
     }
@@ -19,7 +21,14 @@ public class EnnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += 1;
+        if (timer%24==0)
+        {
+            if ((timer/24)%60==0)
+            {
+                print("test");
+            }
+        }
     }
     public void Generate(Ennemy ennemyType, Vector2 position)
     {

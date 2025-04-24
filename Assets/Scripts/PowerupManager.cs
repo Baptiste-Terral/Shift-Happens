8dd2cf3a5cafd9731090c9e369350c0ae5a8657e
bterral <baptiste.terral.auditeur@lecnam.net>
2025-04-24 14:43:39 +0200

    Added a container for powerups in the PowerupManager

--- Assets/Scripts/PowerupManager.cs
+++ Assets/Scripts/PowerupManager.cs
@@ -5,6 +5,7 @@ public class PowerupManager : MonoBehaviour
     [SerializeField] private GameObject[] powerupPrefabs;  // Les différents types de powerups
     [SerializeField] private float spawnInterval = 5f;      // Intervalle entre chaque spawn
     [SerializeField] private float spawnRange = 30f;        // Distance maximale du joueur pour le spawn
+    [SerializeField] private GameObject _powerupsContainer; // Conteneur pour les powerups
 
     private void Start()
     {
@@ -26,6 +27,10 @@ public class PowerupManager : MonoBehaviour
         GameObject powerupPrefab = powerupPrefabs[randomIndex];
 
         // Instanciation du powerup à la position générée
-        Instantiate(powerupPrefab, spawnPosition, Quaternion.identity);
+        GameObject _powerup = Instantiate(powerupPrefab, spawnPosition, Quaternion.identity);
+        
+        var script = _powerup.GetComponent<PowerupPickup>();
+        
+        script.GetComponentInChildren<PowerupPickup>().transform.SetParent(_powerupsContainer.transform);
     }
 }
